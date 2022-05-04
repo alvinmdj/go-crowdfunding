58a974a68f3e5f0432b8ee07e64ee8fdabe6cba9
Alvin <alvinmartindwrk@gmail.com>
2022-05-04 14:57:07 +0700

    create json response with gin

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"go-crowdfunding/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connected to mysql")
+
+	// var users []user.User
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name, user.Email)
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -17,19 +40,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connected to mysql")
-
 	var users []user.User
-
-	length := len(users)
-	fmt.Println(length)
-	
 	db.Find(&users)
-	
-	length = len(users)
-	fmt.Println(length)
 
-	for _, user := range users {
-		fmt.Println(user.Name, user.Email)
-	}
+	c.JSON(http.StatusOK, users)
 }
