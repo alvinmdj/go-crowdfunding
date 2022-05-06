8afb195e2d050f75fcfffb29e58e52e991324401
Alvin <alvinmartindwrk@gmail.com>
2022-05-06 14:27:58 +0700

    create user service to save avatar

--- user/service.go
+++ user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
+	SaveAvatar(id int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -77,4 +78,23 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func (s *service) SaveAvatar(id int, fileLocation string) (User, error) {
+	// get user by id
+	user, err := s.repository.FindById(id)
+	if err != nil {
+		return user, err
+	}
+
+	// update user avatar_file_name
+	user.AvatarFileName = fileLocation
+
+	// save avatar changes to db
+	updatedUser, err := s.repository.Update(user)
+	if  err != nil {
+		return updatedUser, err
+	}
+
+	return updatedUser, nil
 }
