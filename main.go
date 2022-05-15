dfe672ab4e189710ab5757c79c0e08c6a9a45369
Alvin <alvinmartindwrk@gmail.com>
2022-05-15 21:00:22 +0700

    setup transaction folder & file structure

--- main.go
+++ main.go
@@ -19,34 +19,41 @@ import (
 )
 
 func main() {
+	// setup database
 	helper.LoadEnv()
 	dbName := os.Getenv("DB_NAME")
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
 
+	// connect to database MySQL
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	// setup auth
 	authService := auth.NewService()
 
+	// setup user
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 	userHandler := handler.NewUserHandler(userService, authService)
 
+	// setup campaign
 	campaignRepository := campaign.NewRepository(db)
 	campaignService := campaign.NewService(campaignRepository)
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 
+	// setup router
 	router := gin.Default()
 
+	// setup static file routes
 	router.Static("/avatars", "./public/images/avatars")
 	router.Static("/campaign-images", "./public/images/campaign-images")
 
+	// setup api routes
 	api := router.Group("/api/v1")
 
 	// user routes
@@ -65,10 +72,15 @@ func main() {
 	router.Run()
 }
 
+// authMiddleware is a middleware function that checks if the user is authenticated.
+// if user is authenticated, set current user data to context.
+// if user is not authenticated, return status unauthorized & abort request.
 func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// get authorization header
 		authHeader := c.GetHeader("Authorization")
 
+		// check if authorization header is empty, if so, abort request
 		if !strings.Contains(authHeader, "Bearer") {
 			response := helper.APIResponse(
 				"Unauthorized", http.StatusUnauthorized, "error", nil,
@@ -78,8 +90,10 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 			return
 		}
 
+		// get token from authorization header
 		tokenString := strings.Split(authHeader, " ")[1]
 
+		// decode token, if error, abort request
 		token, err := authService.ValidateToken(tokenString)
 		if err != nil {
 			response := helper.APIResponse(
@@ -89,6 +103,7 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 			return
 		}
 
+		// get claims from token, if failed, abort request
 		claim, ok := token.Claims.(jwt.MapClaims)
 		if !ok || !token.Valid {
 			response := helper.APIResponse(
@@ -98,6 +113,8 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 			return
 		}
 
+		// get user id from claims & get user data from database
+		// if failed, abort request
 		userId := int(claim["user_id"].(float64))
 		user, err := userService.GetUserById(userId)
 		if err != nil {
@@ -108,6 +125,7 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 			return
 		}
 
-		c.Set("currentUser", user) // set the current user to the context
+		// set the current user to the context
+		c.Set("currentUser", user)
 	}
 }
