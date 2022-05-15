65539f3f2cba96505a083a10f5b5fa1c5ba9b23f
Alvin <alvinmartindwrk@gmail.com>
2022-05-15 21:26:49 +0700

    Transactions - Get campaign transactions service & input

--- transaction/input.go
+++ transaction/input.go
@@ -1,1 +1,7 @@
 package transaction
+
+// Get campaign transactions input
+// Input Campaign ID from URI parameter
+type GetCampaignTransactionsInput struct {
+	ID int `uri:"id" binding:"required"` // use uri not json
+}
--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,24 @@
 package transaction
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+// Transaction service instance
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+// Service to get transactions by campaign ID
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.FindByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
